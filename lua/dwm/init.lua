c3c4aa5f4f2aec2d3fce0a79cfa6a5678be579d4
delphinus <me@delphinus.dev>
2021-04-08 09:36:19 +0900

    Add new() and reset_master_pane_width()

--- lua/dwm/init.lua
+++ lua/dwm/init.lua
@@ -1,9 +1,45 @@
+local master_pane_width
+
 local M = {}
+
+M.setup = function(opts)
+  vim.validate{
+    opts = {opts, 'table'},
+  }
+  vim.validate{
+    master_pane_width = {
+      opts.master_pane_width,
+      function(v)
+        return type(v) == 'numbfer' or type(v) == 'string' and v:match'^%d+%%$'
+      end,
+      'number (50) or numeber+% (66%)',
+    },
+  }
+  master_pane_width = opts.master_pane_width
+end
+
+--- Open a new window
+-- The master pane move to the top of stacks, and a new window appears.
+-- before:          after:
+--   ┌─────┬─────┐    ┌─────┬─────┐
+--   │     │ S1  │    │     │ S1  │
+--   │     │     │    │     ├─────┤
+--   │     ├─────┤    │     │ S2  │
+--   │  M  │ S2  │    │  M  ├─────┤
+--   │     │     │    │     │ S3  │
+--   │     ├─────┤    │     ├─────┤
+--   │     │ S3  │    │     │ S4  │
+--   └─────┴─────┘    └─────┴─────┘
+M.new = function()
+  M.stack()
+  vim.cmd[[vertical topleft new]]
+  M.resize_master_pane_width()
+end
 
 --- Move the current master pane to the stack
 -- The layout should be the followings.
 --
---   is_clockwise: true  is_clockwise: false
+-- is_clockwise: true  is_clockwise: false
 --   ┌────────┐          ┌────────┐
 --   │   M    │          │   S1   │
 --   ├────────┤          ├────────┤
@@ -13,19 +49,41 @@ local M = {}
 --   ├────────┤          ├────────┤
 --   │   S3   │          │   M    │
 --   └────────┘          └────────┘
--- @param is_clockwise Bool value for the direction.
+-- @param is_clockwise Bool value for the direction. Default: true
 M.stack = function(is_clockwise)
-  vim.api.nvim_set_current_win(M.master_pane_id())
-  local cmd = is_clockwise and 'K' or 'J'
-  vim.cmd('wincmd '..cmd)
-end
-
-M.reset_width = function()
-  vim.cmd[[wincmd =]]
+  is_clockwise = is_clockwise or true
+  vim.cmd(('%dwincmd %s'):format(
+    vim.api.nvim_win_get_number(M.master_pane_id()),
+    is_clockwise and 'K' or 'J'
+  ))
 end
 
 M.master_pane_id = function()
   return vim.api.nvim_list_wins()[1]
+end
+
+M.resize_master_pane_width = function()
+  local width
+  if master_pane_width then
+    if type(master_pane_width) == 'number' then
+      width = master_pane_width
+    else
+      local percentage = tonumber(master_pane_width:match'^(%d+)%%$')
+      width = vim.o.columns * percentage / 100
+    end
+  else
+    width = vim.o.columns / 2
+  end
+
+  local wins = vim.api.nvim_list_wins()
+  local height = vim.o.rows / (#wins - 1)
+  for i, w in ipairs(wins) do
+    if i == 1 then
+      vim.api.nvim_win_set_width(w, width)
+    else
+      vim.api.nvim_win_set_height(w, height)
+    end
+  end
 end
 
 return M
