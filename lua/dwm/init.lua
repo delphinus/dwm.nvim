7acca4f2b0735661d79859b80c2341a72398d6e8
delphinus <me@delphinus.dev>
2021-05-13 21:34:33 +0900

    Add lacked functions

--- lua/dwm/init.lua
+++ lua/dwm/init.lua
@@ -52,14 +52,59 @@ end
 -- @param is_clockwise Bool value for the direction. Default: true
 M.stack = function(is_clockwise)
   is_clockwise = is_clockwise or true
+  local master_pane_id = vim.api.nvim_list_wins()[1]
   vim.cmd(('%dwincmd %s'):format(
-    vim.api.nvim_win_get_number(M.master_pane_id()),
+    vim.api.nvim_win_get_number(master_pane_id),
     is_clockwise and 'K' or 'J'
   ))
 end
 
-M.master_pane_id = function()
-  return vim.api.nvim_list_wins()[1]
+--- Move the current window to the master pane.
+-- The previous master window is added to the top of the stack. If the current
+-- window is in the master pane already, it is moved to the top of the stack.
+M.focus = function()
+  local wins = vim.api.nvim_list_wins()
+  if #wins == 1 then
+    return
+  end
+  local current = vim.api.nvim_get_current_win()
+  if wins[1] == current then
+    vim.cmd[[wincmd w]]
+  end
+  M.stack()
+  vim.api.nvim_exec(([[
+    %dwincmd w
+    wincmd H
+  ]]):format(current), false, {})
+  M.resize_master_pane_width()
+end
+
+--- Handler for BufWinEnter autocmd
+-- Recreate layout broken by the new window
+M.buf_win_enter = function()
+  if #vim.api.nvim_list_wins() == 1 or vim.b.dwm_disabled or
+    not vim.bo.buflisted or vim.bo.filetype == '' or vim.bo.filetype == 'help'
+    or vim.bo.buftype == 'quickfix' then
+    return
+  end
+
+  vim.cmd[[wincmd K]] -- Move the new window to the top of the stack
+  M.focus() -- Focus the new window (twice :)
+  M.focus()
+end
+
+--- Close the current window
+M.close = function()
+  local in_master = vim.fn.winnr() == 1
+  vim.cmd[[close]]
+  if in_master then
+    vim.cmd[[wincmd H]]
+    M.resize_master_pane_width()
+  end
+end
+
+--- Widen the master pane 
+function M.widen_master()
 end
 
 M.resize_master_pane_width = function()
