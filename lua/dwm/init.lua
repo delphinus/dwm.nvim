4b59d2c675e75f803434784768ca7840207bb635
delphinus <me@delphinus.dev>
2021-05-16 12:24:12 +0900

    add setups

--- lua/dwm/init.lua
+++ lua/dwm/init.lua
@@ -1,21 +1,62 @@
+_G.__dwm__funcs__ = {}
 local master_pane_width
 
 local M = {}
 
+-- Setup dwm.nvim
 M.setup = function(opts)
   vim.validate{
     opts = {opts, 'table'},
   }
+  opts = vim.tbl_extend('force', {
+    autocmd = true,
+    key_maps = true,
+    plug_maps = false,
+  }, opts)
   vim.validate{
     master_pane_width = {
       opts.master_pane_width,
       function(v)
-        return type(v) == 'numbfer' or type(v) == 'string' and v:match'^%d+%%$'
+        if not v or type(v) == 'number' then
+          return true
+        end
+        return type(v) == 'string' and v:match'^%d+%%$'
       end,
       'number (50) or numeber+% (66%)',
     },
   }
   master_pane_width = opts.master_pane_width
+
+  -- for backwards compatibility
+  if opts.plug_maps then
+    M.map('<Plug>DWMRotateCounterclockwise', function() M.rotate(false) end)
+    M.map('<Plug>DWMRotateClockwise', function() M.rotate(true) end)
+    M.map('<Plug>DWMNew', M.new)
+    M.map('<Plug>DWMClose', M.close)
+    M.map('<Plug>DWMFocus', M.focus)
+    M.map('<Plug>DWMGrowMaster', function() M.resize_master(1) end)
+    M.map('<Plug>DWMShrinkMaster', function() M.resize_master(-1) end)
+  end
+
+  if opts.key_maps then
+    M.map('<C-,>', function() M.rotate(false) end)
+    M.map('<C-.>', function() M.rotate(true) end)
+    M.map('<C-n>', M.new)
+    M.map('<C-c>', M.close)
+    M.map('<C-@>', M.focus)
+    M.map('<C-Space>', M.focus)
+    M.map('<C-l>', function() M.resize_master(1) end)
+    M.map('<C-h>', function() M.resize_master(-1) end)
+  end
+
+  if opts.autocmd then
+    vim.api.nvim_exec([[
+      augroup dwm.nvim
+        autocmd!
+        autocmd BufWinEnter * lua require'dwm'.buf_win_enter()
+      augroup end
+    ]], false)
+  end
 end
 
 --- Open a new window
@@ -122,6 +163,19 @@ function M.rotate(direction)
   vim.cmd(('wincmd %s'):format(direction and 'W' or 'w'))
   vim.cmd[[wincmd H]]
   M.resize_master()
+end
+
+function M.map(lhs, rhs, opts)
+  opts = vim.tbl_extend('force', {
+    noremap = true,
+    silent = true,
+  }, opts or {})
+  if type(rhs) == 'function' then
+    local next = #_G.__dwm__funcs__ + 1
+    _G.__dwm__funcs__[next] = rhs
+    rhs = (':lua _G.__dwm__funcs__[%d]()'):format(next)
+  end
+  vim.api.nvim_set_keymap('n', lhs, rhs, opts)
 end
 
 M.reset_master = function()
